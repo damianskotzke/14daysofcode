74696e438dabf8938fae071717cbbb46fd264da3
damianskotzke <damianskotzke@gmail.com>
2023-05-15 22:39:32 +0200

    I finally fixed the caret issue

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const editableDiv = document.querySelector('div[contenteditable=true]');
 
 editableDiv.addEventListener('input', function() {
+    console.log("I'm listening");
     const regexBold = /\*(.+?)\*/g;
-    const regexItalic = /\_(.+?)\_/g;
+    // const regexItalic = /\_(.+?)\_/g;
     const content = editableDiv.innerHTML;
 
-    if (/\*(.+?)\*/g.test(content) || /\_(.+?)\_/g.test(content)) {
+    if (/\*(.+?)\*/g.test(content)) {
+        console.log("I start walking");
         const walker = document.createTreeWalker(editableDiv, NodeFilter.SHOW_TEXT);
 
         while (walker.nextNode()) {
+            console.log("I'm walking");
             const node = walker.currentNode;
-            if (node.parentNode.tagName === 'STRONG') {
-                continue;
-            }
+            /*if (node.parentNode.tagName === 'STRONG') {
+                break;
+            }*/
             
             // Detect * and make the content bold
             let match;
             while ((match = regexBold.exec(node.textContent)) !== null) {
+                console.log("I'm looping");
                 const matchStart = match.index;
                 const matchEnd = match.index + match[0].length;
     
@@ -30,31 +34,21 @@ editableDiv.addEventListener('input', function() {
                 const strong = document.createElement('strong');
                 strong.appendChild(bold);
                 node.parentNode.insertBefore(strong, after);
-    
                 node.parentNode.removeChild(node);
     
                 walker.currentNode = after;
-            }
-    
-            // Detect _ and make the content italicised
-            while ((match = regexItalic.exec(node.textContent)) !== null) {
-                const matchStart = match.index;
-                const matchEnd = match.index + match[0].length;
-    
-                const before = document.createTextNode(node.textContent.slice(0,matchStart));
-                const italic = document.createTextNode(node.textContent.slice(matchStart + 1, matchEnd - 1));
-                const after = document.createTextNode(node.textContent.slice(matchEnd));
-    
-                node.parentNode.insertBefore(before, node);
-                node.parentNode.insertBefore(after, node);
-    
-                const em = document.createElement('em');
-                em.appendChild(italic);
-                node.parentNode.insertBefore(em, after);
-    
-                node.parentNode.removeChild(node);
-    
-                walker.currentNode = after;
+
+                // Append zero-width space which moves it outside of the strong tag allow the caret to adjust
+                editableDiv.appendChild(document.createTextNode('\u200B'));
+
+                // Position the caret at the end of the content
+                const selection = window.getSelection();
+                const range = document.createRange();
+                range.selectNodeContents(editableDiv);
+                range.collapse(false); // false means collapse to the end
+                selection.removeAllRanges();
+                selection.addRange(range);
+
             }
         }
     }
